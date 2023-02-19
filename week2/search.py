f4a416fc9ff6922b3eedd96dd49c545016df2c2d
Johannagp9 <johannagp9@gmail.com>
2023-02-19 23:24:54 +0000

    week2

--- week2/search.py
+++ week2/search.py
@@ -62,9 +62,27 @@ def autocomplete():
         prefix = request.args.get("prefix")
         print(f"Prefix: {prefix}")
         if prefix is not None:
-            type = request.args.get("type", "queries") # If type == queries, this is an autocomplete request, else if products, it's an instant search request.
+            type = request.args.get("type", "queries") 
             ##### W2, L3, S1
-            search_response = None
+            index_name =  'bbuy_queries'if type == 'queries' else 'bbuy_products'
+
+            autosuggest_query = {
+                "suggest": 
+                {
+                    "autocomplete": {
+                        "prefix": prefix, 
+                        "completion":{
+                            "field": "suggest",
+                            "skip_duplicates": True
+                        }
+                    }
+
+                }
+            }
+
+            client = get_opensearch()
+            
+            search_response = client.search(body=autosuggest_query, index=index_name)
             print("TODO: implement autocomplete AND instant search")
             if (search_response and search_response['suggest']['autocomplete'] and search_response['suggest']['autocomplete'][0]['length'] > 0): # just a query response
                 results = search_response['suggest']['autocomplete'][0]['options']
@@ -108,6 +126,7 @@ def query():
         ##### W2, L1, S2
 
         ##### W2, L2, S2
+        qu.add_spelling_suggestions(query_obj,user_query)
         print("Plain ol q: %s" % query_obj)
     elif request.method == 'GET':  # Handle the case where there is no query or just loading the page
         user_query = request.args.get("query", "*")
@@ -123,7 +142,7 @@ def query():
         #### W2, L1, S2
 
         ##### W2, L2, S2
-
+        qu.add_spelling_suggestions(query_obj,user_query)
     else:
         query_obj = qu.create_query("*", "", [], sort, sortDir, size=100)
 
